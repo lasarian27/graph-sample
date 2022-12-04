9847517af16ce3f3ef22583353bade2dda8b947f
Duma Cristinel <anonimduma@gmail.com>
2022-12-05 00:01:53 +0200

    first commit

---
+++ graph.js
@@ -0,0 +1,147 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+var graph = require('@microsoft/microsoft-graph-client');
+require('isomorphic-fetch');
+
+module.exports = {
+  getUserDetails: async function(msalClient, userId) {
+    const client = getAuthenticatedClient(msalClient, userId);
+
+    const user = await client
+      .api('/me')
+      .select('displayName,mail,mailboxSettings,userPrincipalName')
+      .get();
+    return user;
+  },
+
+  // <GetCalendarViewSnippet>
+  getCalendarView: async function(msalClient, userId, start, end, timeZone) {
+    const client = getAuthenticatedClient(msalClient, userId);
+
+    return client
+      .api('/me/calendarview')
+      // Add Prefer header to get back times in user's timezone
+      .header('Prefer', `outlook.timezone="${timeZone}"`)
+      // Add the begin and end of the calendar window
+      .query({
+        startDateTime: encodeURIComponent(start),
+        endDateTime: encodeURIComponent(end)
+      })
+      // Get just the properties used by the app
+      .select('subject,organizer,start,end')
+      // Order by start time
+      .orderby('start/dateTime')
+      // Get at most 50 results
+      .top(50)
+      .get();
+  },
+  getEmails: async function(msalClient, userId) {
+// Get the first 10 messages in the user's inbox
+    const client = getAuthenticatedClient(msalClient, userId);
+    return client
+        .api("/me/messages?$filter=isRead eq true")
+        .select('bodyPreview,receivedDateTime,from,toRecipients,subject')
+        .orderby('receivedDateTime desc')
+        .top(10)
+        .get((error, messages) => {
+          console.log(messages)
+          console.log('---------------------DONE--------------------')
+          if (error) {
+            console.log("Error getting messages: " + error);
+          } else {
+            messages.value.forEach((message) => {
+              console.log(`Subject: ${message.subject}`);
+              console.log(`Received: ${message.receivedDateTime.toString()}`);
+              console.log(`From: ${message.from.emailAddress.address}`);
+              console.log(`To: ${JSON.stringify(message.toRecipients)}`);
+              console.log(`Code: ${message.bodyPreview.replace(/\D/g, "")}`);
+            });
+          }
+        });
+  },
+  // </GetCalendarViewSnippet>
+  // <CreateEventSnippet>
+  createEvent: async function(msalClient, userId, formData, timeZone) {
+    const client = getAuthenticatedClient(msalClient, userId);
+
+    // Build a Graph event
+    const newEvent = {
+      subject: formData.subject,
+      start: {
+        dateTime: formData.start,
+        timeZone: timeZone
+      },
+      end: {
+        dateTime: formData.end,
+        timeZone: timeZone
+      },
+      body: {
+        contentType: 'text',
+        content: formData.body
+      }
+    };
+
+    // Add attendees if present
+    if (formData.attendees) {
+      newEvent.attendees = [];
+      formData.attendees.forEach(attendee => {
+        newEvent.attendees.push({
+          type: 'required',
+          emailAddress: {
+            address: attendee
+          }
+        });
+      });
+    }
+
+    // POST /me/events
+    await client
+      .api('/me/events')
+      .post(newEvent);
+  },
+  // </CreateEventSnippet>
+
+};
+
+function getAuthenticatedClient(msalClient, userId) {
+  if (!msalClient || !userId) {
+    throw new Error(
+      `Invalid MSAL state. Client: ${msalClient ? 'present' : 'missing'}, User ID: ${userId ? 'present' : 'missing'}`);
+  }
+
+  // Initialize Graph client
+  const client = graph.Client.init({
+    // Implement an auth provider that gets a token
+    // from the app's MSAL instance
+    authProvider: async (done) => {
+      try {
+        // Get the user's account
+        const account = await msalClient
+          .getTokenCache()
+          .getAccountByHomeId(userId);
+
+        if (account) {
+          // Attempt to get the token silently
+          // This method uses the token cache and
+          // refreshes expired tokens as needed
+          const scopes = process.env.OAUTH_SCOPES || 'https://graph.microsoft.com/.default';
+          const response = await msalClient.acquireTokenSilent({
+            scopes: scopes.split(','),
+            redirectUri: process.env.OAUTH_REDIRECT_URI,
+            account: account
+          });
+
+          // First param to callback is the error,
+          // Set to null in success case
+          done(null, response.accessToken);
+        }
+      } catch (err) {
+        console.log(JSON.stringify(err, Object.getOwnPropertyNames(err)));
+        done(err, null);
+      }
+    }
+  });
+
+  return client;
+}
